8ee215e3f4babcf1f6c2e29d40e5f85fc94e2825
steff456 <steff456@hotmail.com>
2018-04-22 05:00:57 -0500

    parte web

---
+++ react-boilerplate-master/app/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import A from './A';
+import Img from './Img';
+import NavBar from './NavBar';
+import HeaderLink from './HeaderLink';
+import Banner from './banner.jpg';
+import messages from './messages';
+
+class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <A>
+          <Img src={Banner} alt="react-boilerplate - Logo" />
+        </A>
+        <NavBar>
+          <HeaderLink to="/">
+            <FormattedMessage {...messages.home} />
+          </HeaderLink>
+          <HeaderLink to="/features">
+            <FormattedMessage {...messages.features} />
+          </HeaderLink>
+          <HeaderLink to="/demo">
+            <FormattedMessage {...messages.demo} />
+          </HeaderLink>
+        </NavBar>
+      </div>
+    );
+  }
+}
+
+export default Header;
